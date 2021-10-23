e0db90ef43a37446c5463a1197d7f5151338d446
sputgarin <89846079+sputgarin@users.noreply.github.com>
2021-10-23 19:33:24 +0200

    First Commit

---
+++ Assets/Script/forstScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hej hopp din lilla snopp!!")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
